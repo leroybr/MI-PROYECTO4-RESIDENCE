fa68194f0eb7efdcb57bdbd4c6164fecc2ab43de
Le Roy Residence <140985284+leroybr@users.noreply.github.com>
2026-02-23 22:23:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {},
+  },
+  resolve: {
+    alias: {
+      '@': '/',
+    },
+  },
+  // Esto permite que VITE_GOOGLE_API_KEY funcione en Vercel
+  envPrefix: 'VITE_',
+  build: {
+    // Ajusta el límite del tamaño de los chunks a 1000 KB (1 MB)
+    chunkSizeWarningLimit: 1000, // puedes ajustar este valor según lo necesites
+  },
 });
